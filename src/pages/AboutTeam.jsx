2b8fe4ce56cfd5799f50f2c607f2e8707672c3b2
Sruthik Koneti <sruthikkoneti@gmail.com>
2023-07-12 15:08:32 +0530

    initial commit for AboutTeam section

--- src/pages/AboutTeam.jsx
+++ src/pages/AboutTeam.jsx
@@ -1,7 +1,42 @@
-const AboutTeam = () => {
-    return ( <div>
-        
-    </div> );
+import React from 'react'
+function AboutTeam(){
+    return <section id="team" >
+        <div className="teamCard">
+            <img src="" alt="" />
+            <div className="teamInfo">
+                <h4>Name</h4>
+                <p>Role</p>
+            </div>
+        </div>
+        <div className="teamCard">
+            <img src="" alt="" />
+            <div className="teamInfo">
+                <h4>Name</h4>
+                <p>Role</p>
+            </div>
+        </div>
+        <div className="teamCard">
+            <img src="" alt="" />
+            <div className="teamInfo">
+                <h4>Name</h4>
+                <p>Role</p>
+            </div>
+        </div>
+        <div className="teamCard">
+            <img src="" alt="" />
+            <div className="teamInfo">
+                <h4>Name</h4>
+                <p>Role</p>
+            </div>
+        </div>
+        <div className="teamCard">
+            <img src="" alt="" />
+            <div className="teamInfo">
+                <h4>Name</h4>
+                <p>Role</p>
+            </div>
+        </div>
+    </section>
 }
  
 export default AboutTeam;
