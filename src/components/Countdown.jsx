544d2ee10222ade3473d4047ab97f3aff2aedafa
shadoworay <97796821+shadoworay@users.noreply.github.com>
2023-09-11 14:56:39 +0530

    color scheme commit

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -2,37 +2,45 @@
 import { useState, useEffect } from "react";
 import TimerCard from "./TimerCard";
 
-function Countdown(){
+function Countdown() {
+  const endTime = new Date("October 14, 2023 12:00:00.000 GMT+0530").getTime();
+  const [currentTime, setCurrentTime] = useState(new Date().getTime());
+  const gap = endTime - currentTime;
 
-    const endTime= new Date('October 14, 2023 12:00:00').getTime()
-    const [currentTime,setCurrentTime]=useState(new Date().getTime())
-    const gap=endTime-currentTime 
+  const milliseconds = 1;
+  const seconds = 1000 * milliseconds;
+  const minutes = seconds * 60;
+  const hours = minutes * 60;
+  const days = hours * 24;
 
-    const seconds= 1000 
-    const minutes=seconds*60
-    const hours=minutes*60 
-    const days=hours*24 
+  const remainingDays = Math.floor(gap / days);
+  const remainingHours = Math.floor((gap % days) / hours);
+  const remainingMinutes = Math.floor((gap % hours) / minutes);
+  const remainingSeconds = Math.floor((gap % minutes) / seconds);
+  const remainingMilliseconds = Math.floor((gap % seconds) / milliseconds);
 
-    const remainingDays=Math.floor(gap/days)
-    const remainingHours=Math.floor((gap%days)/hours)
-    const remainingMinutes=Math.floor((gap%hours)/minutes)
-    const remainingSeconds=Math.floor((gap%minutes)/seconds)
+  useEffect(() => {
+    setTimeout(() => {
+      setCurrentTime(new Date().getTime());
+    }, 1);
+  }, [currentTime]);
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            setCurrentTime(new Date().getTime())
-        },1000)
-    },[currentTime])
-
-    if(gap<=0){
-        return <div style={{backgroundColor:"black"}}>
-            <h2 style={{color:"pink"}}>Hacking Began!</h2>
-        </div>
-    }
-    else{
-        return <TimerCard days={remainingDays} hours={remainingHours} minutes={remainingMinutes} seconds={remainingSeconds} />
-    }
-   
-
+  if (gap <= 0) {
+    return (
+      <div style={{ backgroundColor: "black" }}>
+        <h2 style={{ color: "pink" }}>Hacking Began!</h2>
+      </div>
+    );
+  } else {
+    return (
+      <TimerCard
+        days={remainingDays}
+        hours={remainingHours}
+        minutes={remainingMinutes}
+        seconds={remainingSeconds}
+        milliseconds={remainingMilliseconds}
+      />
+    );
+  }
 }
-export default Countdown
+export default Countdown;
